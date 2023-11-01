6d19c79c6f44b701c64225c822f7fc6f36a5d77f
RyndyukDanila <ryndyukdanila@gmail.com>
2023-11-01 16:50:35 +0600

    fix - Theme and Resources

--- app/src/main/java/com/example/dotascreen/ui/MainScreen.kt
+++ app/src/main/java/com/example/dotascreen/ui/MainScreen.kt
@@ -2,7 +2,6 @@ package com.example.dotascreen.ui
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -10,7 +9,6 @@ import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.dotascreen.ui.theme.AppTheme
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
@@ -21,7 +19,7 @@ fun MainScreen() {
         ApplySystemBarColors()
 
         Surface (
-            color = AppTheme.BgColors.primary
+            color = AppTheme.BgColors.primary,
             modifier = Modifier.fillMaxSize()
         ) {
             Column (
